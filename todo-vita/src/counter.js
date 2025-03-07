2170e9cd48b295d475c8bd112bc0334a9af8d2ec
Ryo Moriya <ryo.o.3031@gmail.com>
2025-03-08 05:31:28 +0900

    [fix]counter.jsに説明を追加し、jsdocを追加した。

--- todo-vita/src/counter.js
+++ todo-vita/src/counter.js
@@ -1,3 +1,7 @@
+/**
+ * 引数で受け取った要素をクリックするとカウントアップするカウンターをセットアップする
+ * @param {HTMLElement} element クリック時にカウントアップする要素
+ */
 export function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
